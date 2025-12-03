561e087ad3777aa5a72120773ef6f250c41a2b2c
Patricio Alister <tjask24@gmail.com>
2025-12-03 14:26:19 -0300

    Add playRound function
    Added a function that plays one round of the game

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -11,9 +11,45 @@ function getComputerChoice () {
     }
 }
 
-
 function getHumanChoice(){
    let humanChoice = prompt ("Input rock, paper or scissors")
    return humanChoice
 }
 
+let humanScore = 0
+let computerScore = 0
+
+function playRound (humanChoice, computerChoice){
+    humanChoice= humanChoice.toLowerCase()
+
+    if (humanChoice === "rock"){
+        if (computerChoice === "rock"){
+            console.log ("Computer choose Rock. Its a tie!")
+        }
+        else if (computerChoice === "paper"){
+            console.log ("Computer choose Paper.You lose! Paper beats Rock")
+            computerScore++
+        }    
+        else {
+            console.log ("Computer choose Scissors. You win! Rock beats Scissors")
+            humanScore++
+        }
+    }
+    
+    else if (humanChoice === "paper"){
+        if (computerChoice === "paper"){
+            console.log ("Computer choose Paper. Its a tie!")
+        }
+        else if (computerChoice === "rock"){
+            console.log ("Computer choose Rock.You win! Paper beats Rock")
+            humanScore++
+        }    
+        else {
+            console.log ("Computer choose Scissors. You lose! Scissors beat Paper")
+            computerScore++
+        }
+    }
+    console.log (humanScore, computerScore)
+}
+
+playRound(getHumanChoice(),getComputerChoice())
