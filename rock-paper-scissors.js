1a573258de08565fb7a2d9ecd7b1021023260746
Patricio Alister <tjask24@gmail.com>
2025-12-09 19:25:26 -0300

    Create gameEnd function
    The function gameEdnd that manages the messages and buttons
    when the game ends is here.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,18 +1,19 @@
 const btns = document.querySelectorAll("button");
+const buttonContainer = document.getElementById("button-container");
 const results = document.getElementById("results");
 const narrator=document.createElement("p");
 const resultado = document.createElement("p");
-
 let humanScore = 0;
 let computerScore = 0;
-let roundCounter = 1;
+let roundCounter = 0;
+let lastScore=5;
+
 
 btns.forEach(element=>{
     element.addEventListener("click",function (){ 
-    playRound(element.id,getComputerChoice());
-    roundCounter++;
-
-    });
+        roundCounter++;
+        playRound(element.id,getComputerChoice());
+        });
 });
 
 
@@ -27,26 +28,56 @@ function playRound (humanChoice, computerChoice){
     if (humanChoice === computerChoice){
         narrator.textContent=("Computer choose " +  computerChoice+ ". Its a tie!")
     }
-    
+
     else if (
     (humanChoice === "paper" && computerChoice === "rock") ||
     (humanChoice === "rock" && computerChoice === "scissors") ||
     (humanChoice === "scissors" && computerChoice === "paper")) {
-
-        narrator.textContent=("Computer choose "+computerChoice+".You win! " + humanChoice+" beats " +computerChoice +". Well done son.")
+        narrator.textContent=("Computer choose "+computerChoice+".You win! Hell yeah, " + humanChoice+" beats " +computerChoice +". Well done son.")
         humanScore++
     }  
 
     else {
-        narrator.textContent=("Computer choose "+computerChoice+".You loose! " + humanChoice+" beats " +computerChoice+" you clown.")
+        narrator.textContent=("Computer choose "+computerChoice+".You loose! Should have known that " + computerChoice+" beats " +humanChoice+", you clown.")
         computerScore++
     }  
-
 
     results.appendChild(narrator);
     resultado.textContent = "Round " + roundCounter + ". Your current score: " + humanScore + " Computer current score: " + computerScore;
     results.appendChild(resultado);
 
-
+    if ((humanScore==lastScore)||(computerScore==lastScore)){
+       gameEnd();
+    }
 };
 
+ function gameEnd(){
+
+    btns.forEach(element=> element.style.display="none");
+    const newGame = document.createElement("button");
+    newGame.classList.add("button-29")
+    newGame.textContent="Emnpezamos de nuevo?"
+    buttonContainer.appendChild(newGame);
+    const ganador = document.createElement("p")
+    if(humanScore==lastScore){
+        ganador.textContent="Ganaste.... de quien menos lo esperábamos. Felicidades"
+    }
+
+    else {
+        ganador.textContent="PC WINS YOU BAFOOON, HOW COULD YOU EVER THINK YOU COULD WINN"
+    }
+    results.appendChild(ganador)
+
+    newGame.addEventListener("click", function(){
+        btns.forEach(element=>{
+        element.style.display="inline-flex";
+        });
+        ganador.remove();
+        narrator.remove();
+        resultado.remove();
+        newGame.remove();
+    });
+    humanScore=0;
+    computerScore=0;
+    roundCounter=0;
+};
