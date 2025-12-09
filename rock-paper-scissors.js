5088b782d23ce9634abfb20d0cf8faff44b1e5f2
Patricio Alister <tjask24@gmail.com>
2025-12-08 23:37:51 -0300

    Edit playRound and add narrator and resultado
    Changed the function playRound to improve readability,
    added the narrator and resultado parragraphs to show
    current results in the UI

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,82 +1,52 @@
-const piedra = document.getElementById("rock");
-const papel = document.getElementById("paper");
-const tijeras = document.getElementById("scissors");
+const btns = document.querySelectorAll("button");
+const results = document.getElementById("results");
+const narrator=document.createElement("p");
+const resultado = document.createElement("p");
 
 let humanScore = 0;
 let computerScore = 0;
+let roundCounter = 1;
 
-piedra.addEventListener("click",()=>
-    playRound("rock",getComputerChoice())
- );
-papel.addEventListener("click",()=>
-    playRound("paper",getComputerChoice())
- );
-tijeras.addEventListener("click",()=>
-    playRound("scissors",getComputerChoice())
- );
+btns.forEach(element=>{
+    element.addEventListener("click",function (){ 
+    playRound(element.id,getComputerChoice());
+    roundCounter++;
 
+    });
+});
 
 
-
- 
 function getComputerChoice () {
+    const choices = ["rock","paper","scissors"]
     let number = Math.floor(Math.random()*3)
-    if (number === 0) {
-        return ("rock")
-    }
-    else if (number === 1) {
-        return ("paper")
-    }
-    else {
-        return("scissors")
-    }
+    return choices[number];
 };
 
 
-
-
 function playRound (humanChoice, computerChoice){
-    if (humanChoice === "rock"){
-        if (computerChoice === "rock"){
-            console.log ("Computer choose Rock. Its a tie!")
-        }
-        else if (computerChoice === "paper"){
-            console.log ("Computer choose Paper.You lose! Paper beats Rock")
-            computerScore++
-        }    
-        else {
-            console.log ("Computer choose Scissors. You win! Rock beats Scissors")
-            humanScore++
-        }
+    if (humanChoice === computerChoice){
+        narrator.textContent=("Computer choose " +  computerChoice+ ". Its a tie!")
     }
     
-    else if (humanChoice === "paper"){
-        if (computerChoice === "paper"){
-            console.log ("Computer choose Paper. Its a tie!")
-        }
-        else if (computerChoice === "rock"){
-            console.log ("Computer choose Rock.You win! Paper beats Rock")
-            humanScore++
-        }    
-        else {
-            console.log ("Computer choose Scissors. You lose! Scissors beat Paper")
-            computerScore++
-        }
-    }
+    else if (
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "scissors" && computerChoice === "paper")) {
+
+        narrator.textContent=("Computer choose "+computerChoice+".You win! " + humanChoice+" beats " +computerChoice +". Well done son.")
+        humanScore++
+    }  
 
     else {
-        if (computerChoice === "scissors"){
-            console.log ("Computer choose Scissors. Its a tie!")
-        }
-        else if (computerChoice === "paper"){
-            console.log ("Computer choose Paper.You win! Scissors beat Paper")
-            humanScore++
-        }    
-        else {
-            console.log ("Computer choose rock. You loose! Rock beats Scissors")
-            computerScore++
-        }
-    }
-    console.log("Your current score:",humanScore, "Computer current score:",computerScore)
+        narrator.textContent=("Computer choose "+computerChoice+".You loose! " + humanChoice+" beats " +computerChoice+" you clown.")
+        computerScore++
+    }  
+
+
+    results.appendChild(narrator);
+    resultado.textContent = "Round " + roundCounter + ". Your current score: " + humanScore + " Computer current score: " + computerScore;
+    results.appendChild(resultado);
+
+
 };
 
